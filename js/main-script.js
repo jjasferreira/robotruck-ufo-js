433580ddda697cff91f701c0c88023e3959a0a00
João Cardoso <joao.nuno380@gmail.com>
2023-05-04 13:57:52 +0100

    Add scene and basic camera functionality to main script

--- js/main-script.js
+++ js/main-script.js
@@ -2,12 +2,25 @@
 /* GLOBAL VARIABLES */
 //////////////////////
 
+var scene, renderer;
+
+var frontCamera, topCamera, sideCamera, camera;
+
+var tempCube;
+
+var cameraStatus = {front: true, top: false, side: false};
+
 
 /////////////////////
 /* CREATE SCENE(S) */
 /////////////////////
+
 function createScene(){
     'use strict';
+    scene = new THREE.Scene();
+    scene.add(new THREE.AxesHelper(10));
+    scene.background = new THREE.Color(0xF0EAD6);
+    createCube(0, 0, 0);
 
 }
 
@@ -15,6 +28,42 @@ function createScene(){
 /* CREATE CAMERA(S) */
 //////////////////////
 
+function createFrontCamera(){
+    'use strict';
+    frontCamera = new THREE.PerspectiveCamera(70,
+                                              window.innerWidth / window.innerHeight,
+                                              1,
+                                              1000);
+    frontCamera.position.x = 0;
+    frontCamera.position.y = 0;
+    frontCamera.position.z = 50;
+    frontCamera.lookAt(scene.position);
+
+}
+
+function createTopCamera(){
+    'use strict';
+    topCamera = new THREE.PerspectiveCamera(70,
+                                            window.innerWidth / window.innerHeight,
+                                            1,
+                                            1000);
+    topCamera.position.x = 0;
+    topCamera.position.y = 50;
+    topCamera.position.z = 0;
+    topCamera.lookAt(scene.position);
+}
+
+function createSideCamera() {
+    'use strict';
+    sideCamera = new THREE.PerspectiveCamera(70,
+                                                window.innerWidth / window.innerHeight,
+                                                1,
+                                                1000);
+    sideCamera.position.x = 50;
+    sideCamera.position.y = 0;
+    sideCamera.position.z = 0;
+    sideCamera.lookAt(scene.position);
+}
 
 /////////////////////
 /* CREATE LIGHT(S) */
@@ -23,6 +72,17 @@ function createScene(){
 ////////////////////////
 /* CREATE OBJECT3D(S) */
 ////////////////////////
+
+function createCube(x, y, z){
+    'use strict';
+    var geometry = new THREE.BoxGeometry(10, 10, 10);
+    var material = new THREE.MeshBasicMaterial({ color: 0xf0ff0f, wireframe: true });
+    tempCube = new THREE.Mesh(geometry, material);
+    tempCube.position.set(x, y, z);
+    scene.add(tempCube);
+}
+
+
 
 //////////////////////
 /* CHECK COLLISIONS */
@@ -53,7 +113,7 @@ function update(){
 /////////////
 function render() {
     'use strict';
-
+    renderer.render(scene, camera);
 }
 
 ////////////////////////////////
@@ -61,6 +121,23 @@ function render() {
 ////////////////////////////////
 function init() {
     'use strict';
+    renderer = new THREE.WebGLRenderer({
+        antialias: true
+    });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    createScene();
+    createFrontCamera();
+    createTopCamera();
+    createSideCamera();
+    camera = frontCamera;
+
+    render();
+
+    window.addEventListener("keydown", onKeyDown);
+    window.addEventListener("resize", onResize);
+    
 
 }
 
@@ -69,23 +146,48 @@ function init() {
 /////////////////////
 function animate() {
     'use strict';
+    render();
 
+    requestAnimationFrame(animate);
 }
 
 ////////////////////////////
 /* RESIZE WINDOW CALLBACK */
 ////////////////////////////
+
 function onResize() { 
     'use strict';
 
+    // TODO how to keep resize information when changing camera?
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    /* if (window.innerHeight > 0 && window.innerWidth > 0) {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+    } */
 }
 
 ///////////////////////
 /* KEY DOWN CALLBACK */
 ///////////////////////
+ 
 function onKeyDown(e) {
     'use strict';
-
+    switch (e.keyCode) {
+        case 49: // 1 key
+            cameraStatus = {front: true, top: false, side: false};
+            camera = frontCamera;
+            break;
+        case 50: // 2 key
+            cameraStatus = {front: false, top: true, side: false};
+            camera = topCamera;
+            break;
+        case 51: // 3 key
+            cameraStatus = {front: false, top: false, side: true};
+            camera = sideCamera;
+            break;
+    }
 }
 
 ///////////////////////
@@ -93,5 +195,6 @@ function onKeyDown(e) {
 ///////////////////////
 function onKeyUp(e){
     'use strict';
+    //TODO what is the purpose of thisfunction? Is this only applicable to movement or toggles as well?
 
 }
